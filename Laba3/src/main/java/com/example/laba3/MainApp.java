3f05e6b651df043917e0be8f2badf04042e0eaf2
MrSolo501 <mikhinmikhail501@gmail.com>
2024-10-23 21:16:58 +0300

    css

--- Laba3/src/main/java/com/example/laba3/MainApp.java
+++ Laba3/src/main/java/com/example/laba3/MainApp.java
@@ -7,6 +7,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -49,7 +50,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
+        this.primaryStage.getIcons().add(new Image("C:\\Users\\mikha\\IdeaProjects\\Laba3\\src\\main\\resources\\images\\4047337_car_vehicle_transport_van_icon.png"));
         initRootLayout();
 
         showPersonOverview();
